4599b05bc0ce547acb9dbb0d0c2690c5b1155b32
Juan Gomez <ju4ng0mezs@gmail.com>
2024-08-08 16:40:42 -0500

    Add contact

--- app/page.js
+++ app/page.js
@@ -57,14 +57,14 @@ export default function Component() {
                 </div>
                 <div className="flex flex-col gap-2 min-[400px]:flex-row justify-center lg:justify-start">
                   <Link
-                    href="#"
+                    href="https://github.com/xoan04"
                     className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                     prefetch={false}
                   >
                     Learn More
                   </Link>
                   <Link
-                    href="https://github.com/xoan04"
+                    href="https://wa.me/+573245135343"
                     className="inline-flex h-10 items-center justify-center rounded-md border border-input bg-background px-8 text-sm font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                     prefetch={false}
                   >
